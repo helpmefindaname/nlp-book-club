3d5b0d484e646b173d52edff5ab93f1b41eb4bb9
Benedikt Fuchs <benedikt.fuchs.staw@hotmail.com>
2023-07-21 23:39:40 +0200

    add date poll

--- bot/discord.py
+++ bot/discord.py
@@ -1,6 +1,6 @@
 import asyncio
-import contextlib
 import os
+import calendar
 from datetime import datetime, timedelta
 from pathlib import Path
 from typing import Optional
@@ -11,7 +11,7 @@ from discord import TextChannel, Message, EntityType, PrivacyLevel, Guild
 
 from bot.folders import backlog, events, root_path
 from bot.templates import number_emojis, poll_emoji, get_poll_message_text, poll_regex, add_poll_closed_message, \
-    create_event_description
+    create_event_description, date_poll_emoji, get_date_poll_message_text, add_date_poll_closed_message, date_poll_regex
 
 
 class BookclubClient(discord.Client):
@@ -46,9 +46,11 @@ class BookclubClient(discord.Client):
         await self.connect(reconnect=False)
 
     async def on_ready(self):
-        paper, votes = await self.close_poll()
-        await self.announce_event(paper, date=datetime.now().astimezone(pytz.timezone('Europe/Vienna')) + timedelta(days=1))
+        paper, _ = await self.close_poll()
+        date, _ = await self.close_date_poll()
+        await self.announce_event(paper, date=date)
         await self.create_papers_poll()
+        await self.create_date_poll()
         await self.close()
 
     async def create_papers_poll(self):
@@ -65,6 +67,34 @@ class BookclubClient(discord.Client):
             await message.add_reaction(emoji)
         await message.add_reaction(poll_emoji)
 
+    def find_date_candidates(self) -> list[datetime]:
+        cal = calendar.Calendar(0)
+        today = datetime.today()
+        month = cal.monthdatescalendar(today.year, today.month)
+        last_week = month[-1]
+        monday = last_week[0]
+        wednesday = last_week[2]
+        friday = last_week[4]
+        if wednesday.month != today.month:
+            wednesday = wednesday + timedelta(days=-7)
+        if friday.month != today.month:
+            friday = friday + timedelta(days=-7)
+
+        return sorted([datetime(d.year, d.month, d.day) for d in [monday, wednesday, friday]])
+
+    async def create_date_poll(self):
+        an = self.announcements_channel
+        dates = self.find_date_candidates()
+        emojis = number_emojis
+        dates = dates[:len(emojis)]
+        emojis = emojis[:len(dates)]
+        poll_message_text = get_date_poll_message_text(dates, emojis)
+
+        message = await an.send(poll_message_text)
+        for emoji in emojis:
+            await message.add_reaction(emoji)
+        await message.add_reaction(date_poll_emoji)
+
     async def find_poll_message(self) -> Optional[Message]:
         an = self.announcements_channel
         result_msg = None
@@ -74,6 +104,35 @@ class BookclubClient(discord.Client):
             if any(reaction.me and reaction.emoji == poll_emoji for reaction in msg.reactions):
                 result_msg = msg
         return result_msg
+
+    async def find_date_poll_message(self) -> Optional[Message]:
+        an = self.announcements_channel
+        result_msg = None
+        async for msg in an.history(limit=5):
+            if result_msg is not None:
+                continue
+            if any(reaction.me and reaction.emoji == date_poll_emoji for reaction in msg.reactions):
+                result_msg = msg
+        return result_msg
+
+    async def close_date_poll(self) -> tuple[datetime, int]:
+        poll_msg = await self.find_date_poll_message()
+        if poll_msg is None:
+            return datetime(1996, 21, 3), -1
+        emoji_to_votes = {
+            reaction.emoji: reaction.count
+            for reaction in poll_msg.reactions
+            if reaction.me and reaction.emoji in number_emojis
+        }
+        voted_papers = {m["date"]: emoji_to_votes.get(m["emoji"], 0) for m in date_poll_regex.finditer(poll_msg.content)}
+        winning_date_str, winning_votes = max(voted_papers.items(), key=lambda x: x[1])
+        year, month, day = map(int, winning_date_str.split("-"))
+        winning_date = datetime(year, month, day, hour=19, minute=0, tzinfo=pytz.timezone('Europe/Vienna'))
+        await poll_msg.edit(content=add_date_poll_closed_message(
+            poll_msg.content, winning_date, winning_votes)
+        )
+
+        return winning_date, winning_votes
 
     async def close_poll(self) -> tuple[Path, int]:
         poll_msg = await self.find_poll_message()
